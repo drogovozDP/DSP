4782a0c22b510938f58f98ad3b5d7db6f70d666f
drogovozDP <choper2013@bk.ru>
2020-03-13 19:12:41 +1000

    graphic with nice scroll

--- test/engine.js
+++ test/engine.js
@@ -1,40 +1,45 @@
-let ctx, graph;
-let data = 0; 
-let dinamic = 0;       
+let canvas, ctx, graph;
+let dataX = resultX = dinamicX = dataY = resultY = dinamicY = 0; 
 let isMove = false;
 
 function init() {
-    ctx = document.getElementById('draw').getContext('2d');       
-    graph = new Graph(500, 500, 0, 0);    
+    canvas = document.getElementById('graph');
+    ctx = canvas.getContext('2d');
+    graph = new Graph(canvas.width / 2, canvas.height / 2, 0, 0);       
 }
     
 Graph = class
 {
     constructor(x, y, dx, dy)
     {
-        this.size = 10000;
+        this.size = 1000000;
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
-        this.Top = false;
+        this.constX = 0;
+        this.constY = 0;
+        /*this.Top = false;
         this.Bot = false;
         this.Right = false;
-        this.Left = false;            
+        this.Left = false;  */          
     }
     
     create()
     {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
         ctx.beginPath();
-        ctx.moveTo(-this.size + this.dx, 500 / 2 + this.dy);
-        ctx.lineTo(this.size + this.dx, 500/2 + this.dy);
+        ctx.moveTo(-this.size + this.dx + this.constX, this.y + this.dy + this.constY);
+        ctx.lineTo(this.size + this.dx + this.constX, this.y + this.dy + this.constY);
         ctx.stroke();
-        ctx.moveTo(500/2 + this.dx, -this.size + this.dy);
-        ctx.lineTo(500/2 + this.dx, this.size + this.dy);
+        ctx.moveTo(this.x + this.dx + this.constX, -this.size + this.dy + this.constY);
+        ctx.lineTo(this.x + this.dx + this.constX, this.size + this.dy + this.constY);
         ctx.stroke();
         ctx.closePath();
     }
-    
+    /*
     move(isMove, direction) 
     { 
         if (direction == 0) this.Top = isMove;
@@ -50,14 +55,26 @@ Graph = class
         if(this.Bot) this.dy += 1; 
         if(this.Right) this.dx += 1; 
         if(this.Left) this.dx -= 1; 
+    }*/
+    
+    grandFix(dx, dy)
+    {
+        this.dx = dx;
+        this.dy = dy;
     }
+
+    constFix(_resX, _resY)
+    {
+        this.constX += _resX;
+        this.constY += _resY;        
+    }    
 
     makeFunc()
     {
         for(let x = 0; x < 20; x++)
         {
-            ctx.moveTo(500/2 + this.dx + x + 1, 500/2 + this.dy - (x + 1) * (x + 1));    
-            ctx.lineTo(500/2 + this.dx + x + 5, 500/2 + this.dy - (x + 5) * (x + 5));
+            ctx.moveTo(this.x + this.dx + x + 1 + this.constX, this.y + this.dy - (x + 1) * (x + 1) + this.constY);    
+            ctx.lineTo(this.x + this.dx + x + 5 + this.constX, this.y + this.dy - (x + 5) * (x + 5) + this.constY);
             ctx.stroke();
         }
                 
@@ -66,21 +83,44 @@ Graph = class
     
 
 
-/*
+
 function mouseDown() { isMove = true; }
-function mouseUp() { isMove = false; }
+function mouseUp() 
+{
+    isMove = false; 
+
+    resultX = dinamicX - dataX;
+    resultY = dinamicY - dataY;
+
+    graph.constFix(resultX, resultY);    
+    graph.grandFix(0, 0);    
+    
+}
 
 function changeData()
 {
-    if(!isMove) return;
-    data = event.pageX
-}*/
+    if(isMove) 
+    {
+        dinamicX = event.pageX;        
+        dinamicY = event.pageY;
+        graph.grandFix(dinamicX - dataX, dinamicY - dataY)
+        
+    }
+    else
+    {
+        dataX = event.pageX;
+        dataY = event.pageY;
+    }    
+}
 
 function scale(){                        
-    graph.fix()        
+   // graph.fix()        
 
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, 500, 500);
+    document.getElementById('data').innerHTML = dataY;
+    document.getElementById('dinamic').innerHTML = dinamicY;
+    document.getElementById('result').innerHTML = resultY;
+    
+        
     graph.create();     
     graph.makeFunc();
 }
