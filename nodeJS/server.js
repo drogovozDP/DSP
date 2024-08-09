e0c6adf12142d110c00c0e193c6daf32987e02bc
Dima Drogovoz <drogowoz@gmail.com>
2024-08-09 15:10:51 +0400

    remove nodejs

--- nodeJS/server.js
+++
@@ -1,8 +0,0 @@
-let http = require("http");
-
-let server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  res.end('Привет пошел на хуй');
-});
-
-server.listen(1000, 'localhost');
